7d5858ed55806609d9a7269a8bc3588108a9db6e
melrobin <melrobin@gmail.com>
2015-06-11 16:41:08 -0500

    Altering frequency response program and pdf

--- freqresp.cpp
+++ freqresp.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <complex>
+#include <cstdlib>
 
 using namespace std;
 int main(int argc,char *argv[])
@@ -8,14 +9,18 @@ int main(int argc,char *argv[])
    double R=100,C=1e-6,omega;
    complex<double> I,V,Z;
    ofstream outfile;
-    
-   outfile.open("data.txt");
+   if (argc !=2)
+   {
+      printf("Not enough arguments...try again!\n");
+      exit(0);
+   } 
+   outfile.open(argv[1]);
    V=complex<double> (100,0);
-   for (omega=0;omega<100000;omega+=5)
+   for (omega=0;omega<10000;omega+=5)
    {
       Z=complex<double>(R,-1/(omega*C));
       I=V/Z;
-      outfile << omega <<" "<< abs(I) << " "<< arg(I) << endl;
+      outfile << omega <<" "<< abs(I) << " "<< arg(I) << " " << abs(Z) << " " << arg(Z) << endl;
    }
    return(0);
 }
