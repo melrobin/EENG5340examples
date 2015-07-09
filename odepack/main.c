6a3e0095a5dc4fc3d1608c1852603e8eea31f8bb
melrobin <melrobin@gmail.com>
2015-07-09 17:16:14 -0500

    Add examples for ODEPACK and OpenCV

---
+++ odepack/main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* SUBROUTINE DLSODE (F, NEQ, Y, T, TOUT, ITOL, RTOL, ATOL, ITASK,
+ *
+ * ISTATE, IOPT, RWORK, LRW, IWORK, LIW, JAC, MF)
+ * EXTERNAL F, JAC
+ * INTEGER NEQ, ITOL, ITASK, ISTATE, IOPT, LRW, IWORK, LIW, MF
+ * DOUBLE PRECISION Y, T, TOUT, RTOL, ATOL, RWORK
+ * DIMENSION NEQ(*), Y(*), RTOL(*), ATOL(*), RWORK(LRW), IWORK(LIW)
+ *
+ */
+extern void f_(int ,double ,double* ,double* );
+
+extern void dlsode_(void (*)(int *,double*,double *,double *),int*,double *,double *,double *,int *,double *,double *,int *,int*,int*,double *,int*,int *,int*,void (*)(int,double ,double *,int *,int *,double *,int *),int *);
+void jac(int neq,double t,double *y,int *ml,int *mu,double *pd,int *nrowpd)
+{
+  printf("here\n");  
+}
+void g(int *neq,double *t,double *y,double *ydot)
+{
+  double ft,eps=1e-16;
+  
+/* Defines unit step function */
+  if (*t>=0.0)
+     ft=1.0;
+  else
+     ft=0.0;
+  ydot[0]=y[1];
+  ydot[1]=ft-y[0]-3*y[1];
+}
+int main(int argc,char *argv[])
+{
+   int NEQ=2;
+   int MF=10,LRW,LIW,*IWORK,IOPT=0,ITOL=1,ITASK=1,ISTATE=1;
+   int i,j,Nsteps;
+   double *RWORK,t,tout,ATOL,RTOL,y[2]={0,2},delta;
+   FILE *fp;
+
+   Nsteps=atoi(argv[1]);
+   fp=fopen("data.txt","w");/*Our fearless data file */
+   LRW = 20 + 16*NEQ;/*Set up the work arrays */
+   LIW = 20;
+   ATOL=RTOL=1e-3; /*Set up the tolerance*/
+   IWORK=(int *)calloc(LIW,sizeof(int));/*Allocate the arrays */
+   RWORK=(double *)calloc(LRW,sizeof(double));
+   delta=0.1; //set up the resolution of our simulation
+   t=0;
+   tout=t+delta;
+   fprintf(fp,"%f\t%f\t%f\n",t,y[0],y[1]); /*Write the initial conditions */ 
+   for (j=0;j<Nsteps;j++)
+   {
+      dlsode_(g,&NEQ,y,&t,&tout,&ITOL,&RTOL,&ATOL,&ITASK,&ISTATE,&IOPT,RWORK, &LRW,IWORK,&LIW,jac,&MF); /*Call our ODEPACK routine */
+   tout=t+delta;/*Prepare the next end point in the simulation */
+   fprintf(fp,"%f\t%f\t%f\n",t,y[0],y[1]);
+   /*Write the results */
+  }
+   return(0);
+}
--- opencv/display_image.cpp
+++ opencv/display_image.cpp
@@ -22,27 +22,32 @@ int main( int argc, char** argv )
         return -1;
     }
     cout << "The matrix is "<<image.rows << " by " << image.cols << endl;
+//    cout << "The image is "<< image.nchannels << endl;
     int average;
-    double X[image.rows*image.cols];
+    double *X;
+    X=new double[image.rows*image.cols];
     for (int i=00;i<image.rows;i++)
       for (int j=00;j<image.cols;j++)
        {
-           Vec3b intensity = image.at<Vec3b>(i,j);
+           /*Vec3b intensity = image.at<Vec3b>(i,j);
             uchar blue = intensity.val[0];
             uchar green = intensity.val[1];
             uchar red = intensity.val[2];
-            //average=int((blue+green+red)/3);
-            image.at<Vec3b>(i,j).val[0]=uchar(double((2*image.at<Vec3b>(i,j))%255));
-            image.at<Vec3b>(i,j).val[1]=uchar(double((2*image.at<Vec3b>(i,j))%255));;
-            image.at<Vec3b>(i,j).val[2]=uchar(double((2*image.at<Vec3b>(i,j))%255));
-          //  X[i+image.rows*j]=double(average);
+            average=int((blue+green+red)/3);
+            image.at<Vec3b>(i,j).val[0]=image.at<Vec3b>(i,j).val[1]=image.at<Vec3b>(i,j).val[2]=average;*/
+            X[i+image.rows*j]=double(image.at<uchar>(i,j));
      }
     namedWindow("Altered Image",WINDOW_AUTOSIZE);// Create a window for display.
     imshow("Altered Image",image);    // Show our image inside it.
     waitKey(0);   // Wait for a keystroke in the window
-//     ofstream outfile("image_data.txt");
-//     for (int i=0;i<image.rows*image.cols;i++)
-//        outfile<< X[i] << endl;
-//     outfile.close();
+     ofstream outfile("image_data.txt");
+     double sum=0;
+     for (int i=0;i<image.rows*image.cols;i++)
+      {
+        outfile<< X[i] << endl;
+        sum+= X[i];
+      }
+     outfile.close();
+     cout << "The average pixel intensity is " << sum/(image.rows*image.cols) << endl;
     return 0;
 }
